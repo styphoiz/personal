ba5f5ede473680bc60d30a25d6d27e296a7ef31d
Mohammed Hoosain <styphoiz@gmail.com>
2021-06-28 18:01:19 +0200

    entire portfolio

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import { BrowserRouter, Route, Switch} from "react-router-dom";
-import Home from './components/js/Home';
-import About from './components/js/About';
-import SinglePost from './components/js/SinglePost';
-import Post from './components/js/Post';
-import Project from './components/js/Project';
-import NavBar from './components/js/NavBar';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/js/Home";
+import About from "./components/js/About";
+import SinglePost from "./components/js/SinglePost";
+import Post from "./components/js/Post";
+import Project from "./components/js/Project";
+import NavBar from "./components/js/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
-    <NavBar />
+      <NavBar />
       <Switch>
-        <Route component={Home} path='/' exact/>
-        <Route component={About} path='/About' />
-        <Route component={SinglePost} path='/post/:slug' />
-        <Route component={Post} path='/post' />
-        <Route component={Project} path='/project' />
+        <Route component={Home} path="/personal" exact />
+        <Route component={About} path="/About" />
+        <Route component={SinglePost} path="/post/:slug" />
+        <Route component={Post} path="/post" />
+        <Route component={Project} path="/project" />
       </Switch>
     </BrowserRouter>
   );
